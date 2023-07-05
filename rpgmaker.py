785002a316e20c1f98bd322f2d7415e1b014aea1
xannet <xannet@protonmail.com>
2023-07-05 18:09:08 +0300

    added lvl up

--- rpgmaker.py
+++ rpgmaker.py
@@ -4,6 +4,7 @@ from shaman import Shaman
 from mage import Mage
 from druid import Druid
 from hunter import Hunter
+from character import Character
 
 API_TOKEN = '6143855147:AAGhemlrTGrnoqBq_o24oWeBFcjzTxoMU_o'
 
@@ -18,15 +19,15 @@ CHARACTERS = {
     'Маг':Mage,
     }
 
-def handler_filter(message:types.Message, filter_obj:dict) -> bool:
-    for name in filter_obj.keys():
+def handler_filter(message:types.Message) -> bool:
+    for name in CHARACTERS.keys():
         if name == message.text:
             return True
     return False
         
 
         
-def init_village_markup(char):
+def init_village_markup():
     village_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     lvlup_button = types.KeyboardButton('Повысить уровень')
     battle_button = types.KeyboardButton('Отправиться на охоту за монстрами')
@@ -43,8 +44,9 @@ def change_class_markup():
     change_class_markup.add(button_shaman, button_druid, button_hunter, button_mage)
     return change_class_markup
     
-def battle_markup():
+def battle_markup(character:Shaman|Hunter|Druid|Mage):
     battle_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    
     attack = types.KeyboardButton()
     defend = types.KeyboardButton()
     battle_markup.add(attack, defend)
@@ -63,21 +65,26 @@ if __name__ == '__main__':
 )
 
 
-    @bot.message_handler(func=handler_filter(filter_obj=CHARACTERS))
+    @bot.message_handler(func=handler_filter)
     def choose_class(message:types.Message):
         if not is_battle:
             bot.send_message(chat_id=message.chat.id, text=f'Твой выбор: {message.text}', reply_markup=init_village_markup())
+            global character
             character = CHARACTERS[message.text]()
         else:
             bot.send_message(message.chat.id, 'Извини, класc сменить нельзя, пока ты в бою')
             
-    @bot.message_handler(commands=['Повышение уровня'])
-    def level_up(char):
-        char.level_up()
+    @bot.message_handler(func=lambda menu: True if menu.text == 'Повысить уровень' else False)
+    def level_up(message=types.Message):
+        if character:
+            if character.characteristics['exp'] >= 100:
+                bot.send_message(chat_id=message.chat.id, text=character.level_up())
+            else:
+                bot.send_message(chat_id=message.chat.id, text='У тебя пока недостаточно опыта')
         
-    @bot.message_handler(commands=['Защищать деревню!'])
-    def battle(char):
-        pass
+    @bot.message_handler(func=lambda menu: True if menu.text == 'Защищать деревню!' else False)
+    def battle():
+        monster = Character(character.characteristics['lvl']+2)
         
 bot.infinity_polling()
     
