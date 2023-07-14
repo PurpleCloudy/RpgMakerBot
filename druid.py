8c96de81171ffecd69dd8ef54a15ba83bf040ceb
PurpleCloudy <88829502+PurpleCloudy@users.noreply.github.com>
2023-07-14 16:32:08 +0300

    Add files via upload

--- druid.py
+++ druid.py
@@ -1,21 +1,23 @@
 from character import Character
 
+
 class Druid(Character):
     def __init__(self) -> None:
+        super().__init__(lvl=2)  # Проверить соответствие архитектуры класса с SOLID
         self.characteristics = {
-            'max_health':70,
-            'health':70,
-            'power':30,
-            'exp':0,
-            'lvl':1,
-            'crit_chance':7,
-            'def_chance':30,
-            'coefficent':0.1,
-            'cool down':3,
+            'max_health': 70,
+            'health': 70,
+            'power': 30,
+            'exp': 0,
+            'lvl': 1,
+            'crit_chance': 7,
+            'def_chance': 30,
+            'coefficent': 0.1,
+            'cool down': 3,
         }
 
         self.abilities = {
-            'Вызов духов':self.spirit_calling,
+            'Вызов духов': self.spirit_calling,
         }
 
     def spirit_calling(self):
--- hunter.py
+++ hunter.py
@@ -1,21 +1,23 @@
 from character import Character
 
+
 class Hunter(Character):
     def __init__(self) -> None:
+        super().__init__(lvl=1)
         self.characteristics = {
-            'max_health':50,
-            'health':50,
-            'power':50,
-            'exp':0,
-            'lvl':1,
-            'crit_chance':10,
-            'def_chance':50,
-            'coefficent':0.1,
-            'cool down':3,
+            'max_health': 50,
+            'health': 50,
+            'power': 50,
+            'exp': 0,
+            'lvl': 1,
+            'crit_chance': 10,
+            'def_chance': 50,
+            'coefficent': 0.1,
+            'cool down': 3,
         }
 
         self.abilities = {
-            'Увертливость':self.dash,
+            'Увертливость': self.dash,
         }
 
     def dash(self):
